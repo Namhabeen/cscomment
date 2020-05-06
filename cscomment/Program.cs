f737843253a0d7edb90bae99985fbc0781f46920
South_under_been <s2018s27@e-mirim.hs.kr>
2020-05-06 23:41:26 +0900

    초기 커밋.

---
+++ cscomment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscomment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int x, y;
+            /*주석 부분*/
+            Console.Write("X 입력: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Y 입력: ");
+            y = int.Parse(Console.ReadLine());
+            if (x > 4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
